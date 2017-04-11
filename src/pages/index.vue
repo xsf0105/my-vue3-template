a703463fb744c0e4243d1a94328ca222642817a5
徐顺发 <416175716@qq.com>
2017-04-11 18:14:00 +0800

    axios debug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,9 @@
     <div class="container">
         <h1>{{msg}}</h1>
         <button v-on:click="fn1()">点我看控制台</button>
+        <li v-for="item in listArr">
+            <a href="javascript:;">{{item.name}}</a>
+        </li>
     </div>
 </template>
 <script>
@@ -9,33 +12,35 @@
     import axios from 'axios'
 
     export default{
-        data(){
+        data () {
             return{
                 msg:'这个页面包含了使用axios去请求ajax数据和其他事件方法',
-                otherData: '测试',
+                listArr: [],
             }
         },
         created () {
-            console.log("初始化加载数据开始...");
-            axios.get('/json/test.json', {
-              params: {
-
-              }
-            })
-            .then(function (response) {
-              console.log(response);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+            this.init();
         },
         methods: {
-            fn1: function(){
-                console.log("这是点击事件!");
+            init: function() {
+                var _this = this;
+                console.log("初始化加载数据开始...");
+                axios.get('https://api.github.com/repos/typecho-fans/plugins/contents/At', {
+                  params: {
+                    // 需要的参数
+                  }
+                })
+                .then(function (response) {
+                  console.log(response,2);
+                  _this.listArr = response.data;
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
             },
-            // fn2: function(){
-            //     console.log("点击事件2");
-            // }
+            fn1: function() {
+                console.log("这是点击事件!");
+            }
         }
     }
 </script>
@@ -50,6 +55,6 @@
     display: block;
     margin: 0 auto;
     line-height: 30px;
-    /*margin: 60px auto;*/
   }
+
 </style>
