de134e6af357b3f09964e82ab2182d8b4a9ad933
xsf <allan91coder@gmail.com>
2022-01-26 10:36:13 +0800

    Update Page1.vue

--- src/views/Page1.vue
+++ src/views/Page1.vue
@@ -4,19 +4,9 @@
 </template>
 
 <script setup>
-  import { reactive } from 'vue';
+  // import components
   import Chart from '../components/chart.vue'
 
-  // reactive data
-  const state = reactive({ count: 0 })
 </script>
 
-<style>
-  .btn {
-    background-color: #42b983;
-    border-radius: 4px;
-    color: #fff;
-    text-align: center;
-    line-height: 45px;
-  }
-</style>
+<style></style>
